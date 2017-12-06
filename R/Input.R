302a8908ee24ffb63757085798f0d4810d888302
HectorRDB <hector.rouxdebezieux@berkeley.edu>
2017-12-06 15:11:19 -0800

    Modified input

--- R/Input.R
+++ R/Input.R
@@ -6,43 +6,47 @@
 Input <- function(X, Y, ObjectiveFunction, Probs, P, Initialize, mu, Stop, Iterations){
   # Test X
   if( !is.matrix(X) )  stop("X has to be a matrix of numbers", call. = F)  
-  if( !is.numeric(X) ) stop("X has to be a matrix of numbers")  
-  if( ncol(X) < 3) stop("Why are you running a genetic algorithm with that few covariates?!")
+  if( !is.numeric(X) ) stop("X has to be a matrix of numbers", call. = F)  
+  if( ncol(X) < 3) stop("Why are you running a genetic algorithm with that few covariates?!", call. = F)
   
   # test Y
   if( !is.vector(Y) & !(is.matrix(Y))) 
-                       stop("Y has to be a vector or matrix of numbers")
-  if( !is.numeric(Y) ) stop("Y has to be a vector or matrix of numbers")
+                       stop("Y has to be a vector or matrix of numbers", call. = F)
+  if( !is.numeric(Y) ) stop("Y has to be a vector or matrix of numbers", call. = F)
 
-  
+  if( nrow(Y) != nrow(X) ) 
+    stop("X and Y should have the same numer of observations", call. = F)
   
   
   # Test ObjectiveFunction
-  if( typeof(ObjectiveFunction) != "closure" ) stop("ObjectiveFunction has to be a function")
+  if( typeof(ObjectiveFunction) != "closure" ) stop("ObjectiveFunction has to be a function", call. = F)
   
   # Test Probs
-  if( typeof(Probs) != "closure" ) stop("Probs has to be a function")
+  if( typeof(Probs) != "closure" ) stop("Probs has to be a function", call. = F)
   
   # Test P
-  if( length(P) != 1)  stop("Only provides one population size")
-  if( !is.numeric(P) ) stop("P has to be an integer")
-  if( round(P) != P )  stop("P has to be an integer")
-  if( P %% 2 != 0) stop ("The population size has to be even")
+  if( length(P) != 1)  stop("Only provides one population size", call. = F)
+  if( !is.numeric(P) ) stop("P has to be an integer", call. = F)
+  if( round(P) != P )  stop("P has to be an integer", call. = F)
+  if( P %% 2 != 0) stop ("The population size has to be even", call. = F)
   
   # Test Initialize
   if( typeof(Initialize) != "closure" & !is.vector(Initialize) &
       !is.matrix(Initialize)) 
-                       stop("Initialize has to be a vector or matrix")
+                       stop("Initialize has to be a vector or matrix", call. = F)
   
   # Test mu
-  if( length(mu) != 1) stop("Only provides one mutation rate")
-  if( mu > 1 | mu < 0) stop("The mutation rate has to be between 0 and 1")
+  if( length(mu) != 1) stop("Only provides one mutation rate", call. = F)
+  if( mu > 1 | mu < 0) stop("The mutation rate has to be between 0 and 1", call. = F)
   
   # Test Stop
-  if( typeof(Stop) != "closure" ) stop("Stop has to be a function")
+  if( typeof(Stop) != "closure" ) stop("Stop has to be a function", call. = F)
   
   # Test Iterations
-  if( length(Iterations) != 1)  stop("Only provides one maximum number of iterations")
-  if( !is.numeric(Iterations) ) stop("Iterations has to be an integer")
-  if( round(Iterations) != Iterations ) stop("Iterations has to be an integer")
+  if( length(Iterations) != 1)  
+    stop("Only provides one maximum number of iterations", call. = F)
+  if( !is.numeric(Iterations) ) 
+    stop("Iterations has to be an integer", call. = F)
+  if( round(Iterations) != Iterations ) 
+    stop("Iterations has to be an integer", call. = F)
 }
