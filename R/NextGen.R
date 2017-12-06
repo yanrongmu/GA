597eabfae7a7592efe508ea6e43b60947df35a33
yanrongmu <yanrong_mu@berkeley.edu>
2017-12-06 15:07:50 -0800

    small change

--- R/NextGen.R
+++ R/NextGen.R
@@ -2,23 +2,23 @@
 
 ########### Last Updated by Hector 11/27 ###################
 
-source("ParentSelection.R")
-source("Crossover.R")
-source("Mutation.R")
+# source("ParentSelection.R")
+# source("Crossover.R")
+# source("Mutation.R")
 
 NextGen <- function(LastGen, X, Y, ObjectiveFunction, Probs, mu){
   p <- ncol(X)
   P <- nrow(LastGen)
-  
-  # Select the parents 
+
+  # Select the parents
   Parents <- ParentSelection(LastGen, X, Y, ObjectiveFunction, Probs)
-  
+
   # Do Crossover between the Parents
   Kids <- Crossover(Parents)
-  
+
   # Mutate the kids
   Kids <- Mutation(Kids, mu)
-  
+
   #Return the Next Generation
   return(Kids)
 }
--- R/select.R
+++ R/select.R
@@ -2,37 +2,58 @@
 # It performs the genetic algorithm
 
 ########### Last Updated by Hector 11/30 ###################
-library("stats")
-source("Default.R")
-source("Input.R")
-source("Initialize.R")
-source("NextGen.R", chdir = TRUE)
+
+#' select Function
+#'
+#' This function implement a genetic algorithm for variable selection in regression problems.
+#'
+#' @param X A matrix of predictors.
+#' @param Y A vector of responses.
+#' @param ObjectiveFunction A function. Defaults to AIC.
+#' @param Probs A function. Defaults to Ranking.
+#' @param P An integer. Must be even.
+#' @param Initialize A vector or matrix.
+#' @param mu The mutation rate has to be a number between 0 and 1.
+#' @param Stop A function.
+#' @param Iterations An integer.
+#' @return Return the fittest individual in the population.
+#' @importFrom stats.
+#' @export
+#' @examples
+#' select()
+#'
+
+# library("stats")
+# source("Default.R")
+# source("Input.R")
+# source("Initialize.R")
+# source("NextGen.R", chdir = TRUE)
 
 select <- function(X, Y, ObjectiveFunction = AIC, Probs = Ranking,
                    P = 2 * ncol(X), Initialize = Initialize(ncol(X), P),
                    mu = 1 / ncol(X), Stop = Stop, Iterations, ...){
-  
+
   # Lots of checks on the variables
   Input(X, Y, ObjectiveFunction, Probs, P, Initialize, mu, Stop, Iterations)
-  
+
   #Initialize the population
   Gen <- Initialize
-  
+
   # Initialize the stopping criterions
   i <- 0
   Stopping <- FALSE
-  
+
   # Run the algorithm
   while(i < Iterations & !Stopping){
     # Span the next generation
     NewGen <- NextGen(LastGen = Gen, X, Y, ObjectiveFunction, Probs, mu)
-    
+
     # Check if the algorithm stops there
     Stopping <- Stop(X, Y, LastGen = Gen, NewGen, ObjectiveFunction)
     i <- i + 1
     Gen <- NewGen
   }
-  
+
   # Return the fittest individual in the population
   Fitness <- ObjectiveFunction(X, Y, Gen)
   return( Gen[which.max(Fitness), ] )
