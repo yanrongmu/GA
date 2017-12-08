94abc3777e89584f2b9149439d0f81262341fd24
wujoanne <joanne_wu@berkeley.edu>
2017-12-08 15:59:26 -0800

    small edits for nCore

--- R/Input.R
+++ R/Input.R
@@ -3,49 +3,55 @@
 
 ########### Last Updated by Hector 11/30 ###################
 
-Input <- function(X, Y, ObjectiveFunction, Probs, P, Initialized, mu, StopFunction, Iterations){
+Input <- function(X, Y, ObjectiveFunction, Probs, P, Initialized, mu, StopFunction, Iterations, nCores){
   # Test X
-  if( !is.matrix(X) )  stop("X has to be a matrix of numbers", call. = F)  
-  if( !is.numeric(X) ) stop("X has to be a matrix of numbers", call. = F)  
+  if( !is.matrix(X) )  stop("X has to be a matrix of numbers", call. = F)
+  if( !is.numeric(X) ) stop("X has to be a matrix of numbers", call. = F)
   if( ncol(X) < 3) stop("Why are you running a genetic algorithm with that few covariates?!", call. = F)
-  
+
   # test Y
   if(!(is.matrix(Y))) stop("Y has to be a matrix of numbers", call. = F)
   if(!is.numeric(Y))  stop("Y has to be a matrix of numbers", call. = F)
 
-  if( nrow(Y) != nrow(X) ) 
+  if( nrow(Y) != nrow(X) )
     stop("X and Y should have the same number of observations", call. = F)
-  
-  
+
+
   # Test ObjectiveFunction
   if( typeof(ObjectiveFunction) != "closure" ) stop("ObjectiveFunction has to be a function", call. = F)
-  
+
   # Test Probs
   if( typeof(Probs) != "closure" ) stop("Probs has to be a function", call. = F)
-  
+
   # Test P
   if( length(P) != 1)  stop("Only provides one population size", call. = F)
   if( !is.numeric(P) ) stop("P has to be an integer", call. = F)
   if( round(P) != P )  stop("P has to be an integer", call. = F)
   if( P %% 2 != 0) stop ("The population size has to be even", call. = F)
-  
+
   # Test Initialized
   if( typeof(Initialized) != "closure" & !is.vector(Initialized) &
-      !is.matrix(Initialized)) 
+      !is.matrix(Initialized))
                        stop("InitializeFunction has to be a vector or matrix", call. = F)
-  
+
   # Test mu
   if( length(mu) != 1) stop("Only provides one mutation rate", call. = F)
   if( mu > 1 | mu < 0) stop("The mutation rate has to be between 0 and 1", call. = F)
-  
+
   # Test StopFunction
   if( typeof(StopFunction) != "closure" ) stop("StopFunction has to be a function", call. = F)
-  
+
   # Test Iterations
-  if( length(Iterations) != 1)  
+  if( length(Iterations) != 1)
     stop("Only provides one maximum number of iterations", call. = F)
-  if( !is.numeric(Iterations) ) 
+  if( !is.numeric(Iterations) )
     stop("Iterations has to be an integer", call. = F)
-  if( round(Iterations) != Iterations ) 
+  if( round(Iterations) != Iterations )
     stop("Iterations has to be an integer", call. = F)
+
+  # Test nCores
+  if( !is.numeric(nCores))
+    stop("nCores must be an integer", call. = F)
+  if (round(nCores) != nCores )
+    stop("nCores must be an integer", call. = F)
 }
--- tests/testthat/test-select.R
+++ tests/testthat/test-select.R
@@ -7,11 +7,25 @@
 context("Select function")
 
 test_that("Select returns a vector of the fittest individual", {
-  expect_true(is.vector(select(X = matrix(rbinom(200, 1, 0.5), nrow = 20), 
-                        Y = matrix(rbinom(20, 1, 0.5), nrow = 20),
-                        Iterations = 8)))
+  expect_true(is.vector(select(X = matrix(rbinom(200, 1, 0.5), nrow = 20),
+                               Y = matrix(rbinom(20, 1, 0.5), nrow = 20),
+                               ObjectiveFunction = AIC,
+                               Probs = Ranking,
+                               P = 2 * ncol(X),
+                               Initialized = Initialize(ncol(X), P),
+                               mu = 1 / ncol(X),
+                               StopFunction = Stop,
+                               Iterations = 8,
+                               nCores = 1)))
   expect_error(select(X = c("a", "b", "c"),
                       Y = matrix(rbinom(20, 1, 0.5), nrow = 20),
-                      Iterations = 8),
+                      ObjectiveFunction = AIC,
+                      Probs = Ranking,
+                      P = 2 * ncol(X),
+                      Initialized = Initialize(ncol(X), P),
+                      mu = 1 / ncol(X),
+                      StopFunction = Stop,
+                      Iterations = 8,
+                      nCores = 1),
                "X has to be a matrix of numbers")
 })
