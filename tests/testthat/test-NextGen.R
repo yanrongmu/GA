4c10afce3fd1e890d6c6be7df73075c71565adca
wujoanne <joanne_wu@berkeley.edu>
2017-12-08 15:46:53 -0800

    edited nextgen tests

--- tests/testthat/test-NextGen.R
+++ tests/testthat/test-NextGen.R
@@ -6,17 +6,23 @@
 context("NextGen function")
 
 test_that("NextGen produces a matrix of the next generation", {
-  expect_is(NextGen(LastGen = matrix(rbinom(200, 1, 0.5), nrow = 20), 
+  expect_is(NextGen(LastGen = matrix(rbinom(200, 1, 0.5), nrow = 20),
                               X = matrix(rbinom(200, 1, 0.5), nrow = 20),
                               Y = matrix(rbinom(200, 1, 0.5), nrow = 20),
-                              ObjectiveFunction = AIC,
+                              FitnessLastGen = AIC(X = matrix(rbinom(200, 1, 0.5), nrow = 20),
+                                                   Y = matrix(rbinom(20, 1, 0.5), nrow = 20),
+                                                   Gen = matrix(rbinom(200, 1, 0.5), nrow = 20),
+                                                   nCores = 1),
                               Probs = Ranking,
                               mu = 0.5),
               "matrix")
-  expect_equal(dim(NextGen(LastGen = matrix(rbinom(200, 1, 0.5), nrow = 20), 
+  expect_equal(dim(NextGen(LastGen = matrix(rbinom(200, 1, 0.5), nrow = 20),
                     X = matrix(rbinom(200, 1, 0.5), nrow = 20),
                     Y = matrix(rbinom(200, 1, 0.5), nrow = 20),
-                    ObjectiveFunction = AIC,
+                    FitnessLastGen = AIC(X = matrix(rbinom(200, 1, 0.5), nrow = 20),
+                                         Y = matrix(rbinom(20, 1, 0.5), nrow = 20),
+                                         Gen = matrix(rbinom(200, 1, 0.5), nrow = 20),
+                                         nCores = 1),
                     Probs = Ranking,
                     mu = 0.5)),
             c(20,10))
