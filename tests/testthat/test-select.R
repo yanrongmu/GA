a8ed6d6766d18a9a69140e47944c7322aff7e060
wujoanne <joanne_wu@berkeley.edu>
2017-12-12 21:07:16 -0800

    small edit

--- tests/testthat/test-select.R
+++ tests/testthat/test-select.R
@@ -8,20 +8,20 @@ context("Select function")
 
 test_that("Select returns a list of 2", {
   expect_equal(length(select(X = matrix(rbinom(200, 1, 0.5), nrow = 20),
-                               Y = matrix(rbinom(20, 1, 0.5), nrow = 20),
-                               IterationsMax = 8,
-                               nCores = 1)),
+                             Y = matrix(rbinom(20, 1, 0.5), nrow = 20),
+                             IterationsMax = 8,
+                             nCores = 1)),
                2)
 })
 
 test_that("Select returns the last generation and the fittest individual", {
   expect_true(is.matrix(select(X = matrix(rbinom(200, 1, 0.5), nrow = 20),
                                Y = matrix(rbinom(20, 1, 0.5), nrow = 20),
-                               IterationsMax = 8,
+                               IterationsMax = 20,
                                nCores = 1)$FinalGeneration))
   expect_true(is.vector(select(X = matrix(rbinom(200, 1, 0.5), nrow = 20),
                                Y = matrix(rbinom(20, 1, 0.5), nrow = 20),
-                               IterationsMax = 8,
+                               IterationsMax = 20,
                                nCores = 1)$FittestInd))
 })
 
