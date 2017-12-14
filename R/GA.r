c1b6854e6036e5b3cda4c48525b163e31bb77808
yanrongmu <yanrong_mu@berkeley.edu>
2017-12-13 17:10:59 -0800

    add documentation for package itself

---
+++ R/GA.r
@@ -0,0 +1,11 @@
+#' R package: GA
+#'
+#' Implement a genetic algorithm for variable selection in regression problems.
+#'
+#' User can provide matrix of predictors X and matrix of responses Y.
+#' This GA package uses objective function, which defaults to AIC,
+#' to find the fittest variable selection in regression problems.
+#'
+#' @docType package
+#' @name GA
+NULL
